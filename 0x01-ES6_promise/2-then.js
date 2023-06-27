0dbafed1c3baf5ae6c20f4f0dcdc5ed4d769c8ca
mondlivundla <mondlivundla7@gmail.com>
2023-06-27 12:55:08 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return Promise
     .then(() => ({status: 200, body: 'success',}))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI
