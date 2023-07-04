65d43ec89edb4fc37f6461bdc047777f1490b7d1
mondlivundla <mondlivundla7@gmail.com>
2023-07-04 17:46:23 +0200

    data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -5,7 +5,7 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Mondliwethu Vundla>
  * @returns {Number}
  */
 export default function getStudentIdsSum(students) {
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Mondliwethu Vundla>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
